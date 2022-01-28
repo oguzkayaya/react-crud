69fae6ffebafdada04026ddec3726ab8de14bf58
oguz kaya <oguz.kaya@etstur.com>
2022-01-28 23:50:36 +0300

    profile page completed

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -18,12 +18,41 @@ function Profile() {
 
     return (
         <div className="profile">
-            <div>{userInfo.id}</div>
-            <div>{userInfo.email}</div>
-            <div>{userInfo.name}</div>
-            <div>{userInfo.phone}</div>
-            <div>{userInfo.username}</div>
-            <div>{userInfo.website}</div>
+            <div className="name">{userInfo.name}</div>
+            <div className="city">{userInfo.address?.city}</div>
+
+            <div className="user-info">
+                <div className="label">Username</div>
+                <div className="value">{userInfo.username}</div>
+            </div>
+            <div className="user-info">
+                <div className="label">Email</div>
+                <div className="value">{userInfo.email}</div>
+            </div>
+            <div className="user-info">
+                <div className="label">Phone</div>
+                <div className="value">{userInfo.phone}</div>
+            </div>
+            <div className="user-info">
+                <div className="label">Website</div>
+                <div className="value"><a href="">{userInfo.website}</a></div>
+            </div>
+            <div className="user-info">
+                <div className="label">Company</div>
+                <div className="value">{userInfo.company?.name}</div>
+            </div>
+
+            <div className="line"></div>
+
+
+            <div className="profile-bottom-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Ut ea nostrum numquam officiis quas commodi dolores nobis eaque veniam quos impedit sit,
+                eligendi architecto cum vitae minima suscipit cupiditate iure ratione asperiores rerum atque aperiam deserunt est.
+            </div>
+            <div className="profile-show-more">
+                Show more
+            </div>
         </div>
     )
 }
