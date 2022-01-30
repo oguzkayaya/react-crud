8cee044dd71271292a3141b2a273729def405219
oguz kaya <oguz.kaya@etstur.com>
2022-01-30 16:17:54 +0300

    added delete modal

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import "./Posts.css";
+import closeIcon from "../../close-icon.png";
 
 
 function Posts() {
@@ -8,12 +9,14 @@ function Posts() {
 
     const [posts, setPosts] = useState([]);
 
+    const [showDetail, setShowDetail] = useState(false);
     const [updatedId, setUpdatedId] = useState("");
     const [updatedTitle, setUpdatedTitle] = useState("");
     const [updatedBody, setUpdatedBody] = useState("");
     const [updatedUserId, setUpdatedUserId] = useState("");
 
-    const [showDetail, setShowDetail] = useState(false);
+    const [showDelete, setShowDelete] = useState(false);
+    const [deletedPost, setDeletedPost] = useState({});
 
     useEffect(() => {
         fetch('http://jsonplaceholder.typicode.com/posts')
@@ -33,6 +36,11 @@ function Posts() {
         setUpdatedUserId(post.userId);
     }
 
+    const openDeleteModal = (post) => {
+        setShowDelete(true);
+        setDeletedPost(post);
+    }
+
     const updatePost = () => {
         fetch(`https://jsonplaceholder.typicode.com/posts/${updatedId}`, {
             method: 'PUT',
@@ -45,18 +53,31 @@ function Posts() {
             headers: {
                 'Content-type': 'application/json; charset=UTF-8',
             },
-        })
+        }
+        )
             .then((response) => response.json())
             .then((json) => {
                 setPosts(posts.map((post) => {
-                    if(post.id == json.id) {
+                    if (post.id == json.id) {
                         return json;
                     }
                     return post;
                 }));
                 setShowDetail(false);
-            }
-            );
+            });
+    }
+
+    const deletePost = () => {
+        fetch(`https://jsonplaceholder.typicode.com/posts/${deletedPost.id}`, {
+            method: 'DELETE',
+        })
+            .then(response => {
+                if (response.ok) {
+                    setPosts(posts.filter(post => post.id != deletedPost.id));
+                    setShowDelete(false);
+                }
+            });
+
     }
 
     return (
@@ -69,14 +90,14 @@ function Posts() {
                         <div className="post-buttons">
                             <button className="btn-info" onClick={() => { navigate(`/post/${post.id}`) }}>DETAY</button>
                             <button className="btn-success" onClick={() => { openUpdateModal(post) }}>DÜZENLE</button>
-                            <button className="btn-danger">SİL</button>
+                            <button className="btn-danger" onClick={() => { openDeleteModal(post) }}>SİL</button>
                         </div>
                     </div>
                 ))}
             </div>
             {showDetail && (
                 <div className="detail-modal">
-                    <div className="modal-title">Düzenle</div>
+                    <div className="modal-title">Düzenle <img src={closeIcon} alt="" className="modal-close-icon" onClick={() => { setShowDetail(false) }} /></div>
                     <div className="modal-body">
                         <div className="modal-row">
                             <div className="modal-label">
@@ -100,6 +121,21 @@ function Posts() {
                     </div>
                 </div>
             )}
+            {
+                showDelete && (
+                    <div className="delete-modal">
+                        <div className="modal-title">Sil <img src={closeIcon} alt="" className="modal-close-icon" onClick={() => { setShowDelete(false) }} /></div>
+                        <div className="modal-body">
+                            <div className="post-id">{deletedPost.id}</div>
+                            <div className="post-title">{deletedPost.title}</div>
+                        </div>
+                        <div className="modal-footer">
+                            <button className="btn-danger" onClick={deletePost}>Sil</button>
+                            <button className="btn-update" onClick={() => { setShowDelete(false) }}>İptal</button>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     )
 }
